a579716391ed3b20db6ec6e7c7e3a2600aafec74
siam114 <smsiamislam598@gmail.com>
2025-05-05 12:42:17 +0600

    dispatch action

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// defining const
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_USER = 'ADD_USER';
+
 //state
 const initialCounterState = {
     count: 0,
@@ -7,4 +12,22 @@ const initialUserState = {
     users: [{name: 'Siam'}],
 };
 
-//action
+//action - object -type and payload
+const incrementCounter = () =>{
+    return {
+        type: INCREMENT,
+    };
+}
+
+const decrementCounter = () =>{
+    return {
+        type: DECREMENT,
+    };
+}
+
+// const addUser = (user) =>{
+//     return {
+//         type: ADD_USER,
+//         payload: user,
+//     };
+// }
