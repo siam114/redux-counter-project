c8d625eba3c7251ea6698c552b261000de1e6b27
siam114 <smsiamislam598@gmail.com>
2025-05-07 01:33:10 +0600

    fetch reducer define

--- fetch.js
+++ fetch.js
@@ -31,3 +31,28 @@ const getTodosFailure = (error) =>{
     }
 }
 
+
+//reducer
+const todosReducer = (state=initialTodosState , action) =>{
+    switch(action.type) {
+        case GET_TODOS_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+            }
+
+        case GET_TODOS_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                todos: action.payload,
+            }
+
+        case GET_TODOS_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            }
+    }
+}
