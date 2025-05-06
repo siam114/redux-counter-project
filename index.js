447bf8c36d28e44143ab75457558bc5c8671dda2
siam114 <smsiamislam598@gmail.com>
2025-05-06 12:41:03 +0600

    store ans store define

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const {createStore} = require('redux');
+
 // defining const
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
@@ -48,3 +50,14 @@ const counderReducer = (state = initialCounterState, action) =>{
             }
     }
 }
+
+//create store - takes reducer and returns store
+const store = createStore(counderReducer);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+//dispatch action - call the action and pass it to the store
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
